bfcc349bb5452402464dc5936c2c10364213ce38
GPonomarev <pvg9kz@gmail.com>
2022-06-04 15:14:11 +0600

    change project

---
+++ _next/static/chunks/546.97a172fff00c499c.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[546],{8546:function(o,e,l){l.r(e);var r=l(949),a=l(5193),c=l(9876),k=l(5893);e.default=function(){var o=(0,r.If)(),e=o.colorMode,l=o.toggleColorMode;return(0,k.jsx)(a.hU,{"aria-label":"Toggle dark mode",icon:"dark"===e?(0,k.jsx)(c.NW,{}):(0,k.jsx)(c.kL,{}),onClick:l,color:{light:"black",dark:"white"}[e]})}}}]);
